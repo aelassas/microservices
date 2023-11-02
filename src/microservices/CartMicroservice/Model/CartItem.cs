c5de833028fbe2291e9c526cbce8bbc6000302bc
aelassas <akram.elassas@gmail.com>
2023-11-02 17:56:56 +0100

    Update CartItem.cs

--- src/microservices/CartMicroservice/Model/CartItem.cs
+++ src/microservices/CartMicroservice/Model/CartItem.cs
@@ -6,7 +6,7 @@ namespace CartMicroservice.Model;
 public class CartItem
 {
     [BsonRepresentation(BsonType.ObjectId)]
-    public string? CatalogItemId { get; set; }
+    public string? CatalogItemId { get; init; }
     public required string Name { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
